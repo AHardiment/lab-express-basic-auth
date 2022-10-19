7982206a716471cb4a28a04aff6343b5a5009be4
AHardiment <99344068+AHardiment@users.noreply.github.com>
2022-10-19 10:26:23 +0100

    completed all mandatory iterations

--- app.js
+++ app.js
@@ -1,35 +1,54 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require('dotenv/config');
+require("dotenv/config");
 
 // ℹ️ Connects to the database
-require('./db');
+require("./db");
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require('express');
+const express = require("express");
 
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
-const hbs = require('hbs');
-
+const hbs = require("hbs");
 const app = express();
+const expressSession = require("express-session");
+const MongoStore = require("connect-mongo");
+const User = require("./models/User.model");
+const deserializeUserMiddleware = require("./middleware/deserialize-user-middleware");
+
+app.use(
+  expressSession({
+    secret: "abcde",
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 15,
+    },
+    store: new MongoStore({
+      mongoUrl: process.env.MONGODB_URI,
+      ttl: 60 * 60 * 24,
+    }),
+  })
+);
+app.use(deserializeUserMiddleware);
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
-require('./config')(app);
+require("./config")(app);
 
 // default value for title local
-const projectName = 'lab-express-basic-auth';
-const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
+const projectName = "lab-express-basic-auth";
+const capitalized = (string) =>
+  string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
 // 👇 Start handling routes here
-const index = require('./routes/index');
-app.use('/', index);
+const index = require("./routes/index");
+app.use("/", index);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require('./error-handling')(app);
+require("./error-handling")(app);
 
 module.exports = app;
-
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,76 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
+const User = require("./../models/User.model");
+const bcryptjs = require("bcryptjs");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  res.render("home");
+});
+
+router.get("/main", (req, res, next) => {
+  res.render("main");
+});
+
+router.get("/log-in", (req, res, next) => {
+  res.render("log-in");
+});
+
+router.get("/register", (req, res, next) => {
+  res.render("register");
+});
+
+router.get("/private", (req, res, next) => {
+  res.render("private");
+});
+
+router.post("/register", (req, res, next) => {
+  const { username, password } = req.body;
+  bcryptjs
+    .hash(password, 10)
+    .then((hashAndSalt) => {
+      User.create({
+        username,
+        passwordHashAndSalt: hashAndSalt,
+      });
+    })
+    .then((user) => {
+      req.session.userId = user._id;
+      res.redirect("/");
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.post("/log-in", (req, res, next) => {
+  const { username, password } = req.body;
+  let user;
+  User.findOne({ username })
+    .then((userDocument) => {
+      user = userDocument;
+      if (user) {
+        return bcryptjs.compare(password, user.passwordHashAndSalt);
+      } else {
+        return false;
+      }
+    })
+    .then((result) => {
+      if (result) {
+        req.session.userId = user._id;
+        res.redirect("/main");
+      } else {
+        res.redirect("/log-in");
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.post("/log-out", (req, res, next) => {
+  req.session.destroy();
+  res.redirect("/");
 });
 
 module.exports = router;
